9e04e48abe2fa499acb3d473429f34f73e650053
Kyle Mathews <mathews.kyle@gmail.com>
2016-12-11 08:51:32 -0800

    Match mailto and data schemes - fixes #4 (#5)

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = function (url) {
 		throw new TypeError('Expected a string');
 	}
 
-	return /^(?:\w+:)\/\//.test(url);
+	return /^[a-z][a-z0-9+.-]*:/.test(url);
 };
--- test.js
+++ test.js
@@ -6,6 +6,8 @@ it('should match absolute urls', function () {
 	assert(isAbsoluteUrl('http://sindresorhus.com'));
 	assert(isAbsoluteUrl('https://sindresorhus.com'));
 	assert(isAbsoluteUrl('file://sindresorhus.com'));
+	assert(isAbsoluteUrl('mailto:someone@example.com'));
+	assert(isAbsoluteUrl('data:text/plain;base64,SGVsbG8sIFdvcmxkIQ%3D%3D'));
 	assert(!isAbsoluteUrl('//sindresorhus.com'));
 	assert(!isAbsoluteUrl('/foo/bar'));
 	assert(!isAbsoluteUrl('foo/bar'));
