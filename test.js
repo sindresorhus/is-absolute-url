77bb5010ae524667054d820b130c2cae5b52f4ea
Alex Brasetvik <alex@brasetvik.com>
2019-09-27 20:15:26 +0200

    Ignore case when testing scheme

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ import isAbsoluteUrl from '.';
 test('main', t => {
 	t.true(isAbsoluteUrl('http://sindresorhus.com'));
 	t.true(isAbsoluteUrl('https://sindresorhus.com'));
+	t.true(isAbsoluteUrl('httpS://sindresorhus.com'));
 	t.true(isAbsoluteUrl('file://sindresorhus.com'));
 	t.true(isAbsoluteUrl('mailto:someone@example.com'));
 	t.true(isAbsoluteUrl('data:text/plain;base64,SGVsbG8sIFdvcmxkIQ%3D%3D'));
