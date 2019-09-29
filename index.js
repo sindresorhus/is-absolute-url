17cbebc9096e33e67e2e845d670782200456f32c
Alex Brasetvik <alex@brasetvik.com>
2019-09-29 07:30:37 +0200

    Ignore case when testing scheme (#9)

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ module.exports = url => {
 	}
 
 	// Don't match Windows paths `c:\`
-	if (/^[a-z]:\\/.test(url)) {
+	if (/^[a-zA-Z]:\\/.test(url)) {
 		return false;
 	}
 
 	// Scheme: https://tools.ietf.org/html/rfc3986#section-3.1
 	// Absolute URL: https://tools.ietf.org/html/rfc3986#section-4.3
-	return /^[a-z][a-z\d+\-.]*:/.test(url);
+	return /^[a-zA-Z][a-zA-Z\d+\-.]*:/.test(url);
 };
