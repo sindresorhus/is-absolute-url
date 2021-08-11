48c3c32cf5864dc399d4c47c64700c94451706ae
Dennis Morello <dennismorello@gmail.com>
2021-08-11 07:18:00 +0000

    fix(regex): extract the regex declaration outside the function #15

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// Scheme: https://tools.ietf.org/html/rfc3986#section-3.1
+// Absolute URL: https://tools.ietf.org/html/rfc3986#section-4.3
+const ABSOLUTE_URL_REGEX = /^[a-zA-Z][a-zA-Z\d+\-.]*?:/;
+
+// Windows paths like `c:\`
+const WINDOWS_PATH_REGEX = /^[a-zA-Z]:\\/;
+
 export default function isAbsoluteUrl(url) {
 	if (typeof url !== 'string') {
 		throw new TypeError(`Expected a \`string\`, got \`${typeof url}\``);
 	}
 
-	// Don't match Windows paths `c:\`.
-	if (/^[a-zA-Z]:\\/.test(url)) {
+	if (WINDOWS_PATH_REGEX.test(url)) {
 		return false;
 	}
 
-	// Scheme: https://tools.ietf.org/html/rfc3986#section-3.1
-	// Absolute URL: https://tools.ietf.org/html/rfc3986#section-4.3
-	return /^[a-zA-Z][a-zA-Z\d+\-.]*?:/.test(url);
+	return ABSOLUTE_URL_REGEX.test(url);
 }
